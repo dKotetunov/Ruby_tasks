443570006cac85a95ba318efe1220912bee76bed
dkotetunov <dkotetunov@gmail.com>
2014-11-03 23:01:34 +0200

    4th task

--- task4.rb
+++ task4.rb
@@ -1,21 +1,20 @@
 class Task4
-def initialize
-@hash = {'yes' => 23, 'b' => 'travel', 'yesterday' => 34,
-        5 => '234', :yesss => :fg,
-        try: {'yesterday1'=> 34, 'yesteryear' => 2014},
-        key: [{'yes' => 23, 'b' => 'travel'}],
-        'yesterday1'=> 34, 'yesteryear' => 2014}
-end
-
-
-  def keys_values
-    @hash.inject({}){|result, (key, value)|
+  def initialize
+    hash = {'yes' => 23, 'b' => 'travel', 'yesterday' => 34,
+            5 => '234', :yesss => :fg,
+            try: {'yesterday1'=> 34, 'yesteryear' => 2014},
+            key: [{'yes' => 23, 'b' => 'travel'}],
+            'yesterday1'=> 34, 'yesteryear' => 2014}
+    puts keys_values(hash)
+  end
+  def keys_values(hash1)
+    hash1.inject({}){|result, (key, value)|
       new_key = case key
                   when String then key.to_sym
                   else key
                 end
       new_value = case value
-                    when Hash then keys_values
+                    when Hash then keys_values value
                     else value
                   end
       result[new_key] = new_value
@@ -23,7 +22,5 @@ end
     }
   end
 end
-puts "Symbolized hash:"
-change_keys = Task4.new
-puts change_keys.keys_values
 
+Task4.new
